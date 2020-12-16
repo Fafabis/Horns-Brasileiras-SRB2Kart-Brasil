41a739f228715064eb0c27abc8945c19d4a55280
Fabiano0Br <fabianobr157@gmail.com>
2020-12-16 11:16:40 -0300

    HornsBR V8

--- pack_pk3.sh
+++ pack_pk3.sh
@@ -5,5 +5,5 @@ if [ ! -f convertpk3-src/convertpk3 ]; then
     cd ..
 fi
 
-PK3NAME="KL_HornsBR_v7.1" #rename to later version
+PK3NAME="KL_HornsBR_v8" #rename to later version
 convertpk3-src/convertpk3 . $PK3NAME.pk3
